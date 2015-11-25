e2a1015273687cf0be5547f59fafaae6fe615974
truextcfans <583284407@qq.com>
2015-11-25 12:14:57 +0800

    Initial Commit

---
+++ 波形图/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  波形图
+//
+//  Created by 大地创想 on 15/11/25.
+//  Copyright © 2015年 大地创想. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
